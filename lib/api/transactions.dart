3518174c7e9a8f8e0bdcbf9df80e05688359df49
Andhika Effendy <andhikaeffendy14@gmail.com>
2021-02-10 22:18:10 +0700

    full api transaction

--- lib/api/transactions.dart
+++ lib/api/transactions.dart
@@ -17,6 +17,43 @@ Future<ApiTransaction> futureApiTransaction(String token, int userId) async{
   return ApiTransaction.fromStringJson(response.toString());
 }
 
+Future<ApiTransaction> futureApiActiveTransaction(String token, int userId) async{
+  var dio = Dio();
+  String url = api_url + "active_transactions_list?user_id=" + userId.toString();
+  dio.options.headers[HttpHeaders.authorizationHeader] =
+      'Bearer ' + token;
+  Response response = await dio.get(url);
+  print("response : " + response.toString());
+  print(response.data);
+
+  return ApiTransaction.fromStringJson(response.toString());
+}
+
+Future<ApiTransaction> futureApiPastTransaction(String token, int userId) async{
+  var dio = Dio();
+  String url = api_url + "past_transactions_list?user_id=" + userId.toString();
+  dio.options.headers[HttpHeaders.authorizationHeader] =
+      'Bearer ' + token;
+  Response response = await dio.get(url);
+  print("response : " + response.toString());
+  print(response.data);
+
+  return ApiTransaction.fromStringJson(response.toString());
+}
+
+Future<ApiTransaction> futureApiTransactionDetail(String token, int id) async{
+  var dio = Dio();
+  String url = api_url + "transaction_detail?id=" + id.toString();
+  dio.options.headers[HttpHeaders.authorizationHeader] =
+      'Bearer ' + token;
+  Response response = await dio.get(url);
+  print("response : " + response.toString());
+  print(response.data);
+
+  return ApiTransaction.fromStringJson(response.toString());
+}
+
+
 class ApiTransaction {
   String status;
   String message;
